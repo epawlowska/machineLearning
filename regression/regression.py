efe8fc1860c7b355770e84ac6156fef36ecd0a08
Ewa <Ewa>
2015-11-12 22:08:07 +0100

    Hash trick for float.

--- regression/regression.py
+++ regression/regression.py
@@ -36,7 +36,6 @@ def generate_logistic_csv_data(input_file_name, n=30, a=[1, 2, 0], b=[-5, 0, 0],
     for i in range(data.shape[0]):
         data[i, m - 1] = sum(c[j] * data[i, j] for j in range(3)) + 10 * random.gauss(0, 0.7)
         data[i, m - 1] = 0.0 if data[i, m - 1] > 0 else 1.0
-        print data[i, m - 1]
 
     df = pandas.DataFrame(data)
     df.to_csv(input_file_name, index=False)
@@ -44,7 +43,7 @@ def generate_logistic_csv_data(input_file_name, n=30, a=[1, 2, 0], b=[-5, 0, 0],
 
 file_name = "data.csv"
 samples_count = 30
-generate_logistic_csv_data(file_name, samples_count, [1, 2, 0], [-5, 0, 0], [10, 1, 3])
+generate_csv_data(file_name, samples_count, [1, 2, 0], [-5, 0, 0], [10, 1, 3])
 data = get_data_from_csv(file_name)
 
 #20, 009, 0,999, without standardization
@@ -57,24 +56,61 @@ class Regression:
     batch_size = 5
     n_iter = 10000
     shuffle = True
-    holdout_size_coef = 0.0
+    holdout_size_coef = 0.2
     l2 = 0.0
     standardization = True
-    loss_method = " " #SQUARED
+    loss_method = SQUARED
+    use_hash_trick = True
 
-    initial_input = np.ones(data.shape, dtype=np.float)
-    initial_input[:, 1:] = data[:, :-1]
-    initial_output = data[:, -1:].transpose(1, 0)[0]
+    initial_input = None
+    initial_output = None
 
-    gradient = np.zeros(initial_input.shape[1], dtype=np.float)
-    predictor = gradient
+    learning_input = None
+    learning_output = None
+    validation_input = None
+    validation_output = None
 
-    holdout_size = int(holdout_size_coef * initial_input.shape[0])
-    random_rows = random.sample(range(initial_input.shape[0]), holdout_size)
-    validation_input = initial_input[random_rows, :]
-    validation_output = initial_output[random_rows]
-    initial_input = scipy.delete(initial_input, random_rows, 0)
-    initial_output = scipy.delete(initial_output, random_rows)
+    w = None
+
+    def __init__(self):
+        self.prepare_learning_data()
+
+    def prepare_learning_data(self):
+        self.initial_input = np.ones(data.shape, dtype=np.float)
+        self.initial_input[:, 1:] = data[:, :-1]
+        self.initial_output = data[:, -1:].transpose(1, 0)[0]
+
+        self.extract_validation_data()
+
+        self.learning_input = np.array(self.initial_input)
+        self.learning_output = np.array(self.initial_output) #not necessary
+
+        if self.use_hash_trick:
+            self.learning_input = self.hash_trick(self.learning_input, 1)
+            self.validation_input = self.hash_trick(self.validation_input, 1)
+
+        if self.standardization:
+            self.standardize(self.learning_input)
+
+        self.w = np.zeros(self.learning_input.shape[1], dtype=np.float)
+
+
+    def extract_validation_data(self):
+        holdout_size = int(self.holdout_size_coef * self.initial_input.shape[0])
+        random_rows = random.sample(range(self.initial_input.shape[0]), holdout_size)
+        self.validation_input = self.initial_input[random_rows, :]
+        self.validation_output = self.initial_output[random_rows]
+        self.initial_input = scipy.delete(self.initial_input, random_rows, 0)
+        self.initial_output = scipy.delete(self.initial_output, random_rows)
+
+    def hash_trick(self, x, n):
+        new_x = np.zeros((x.shape[0], n + 1), dtype=np.float)
+        new_x[:, 0] = 1
+        for i in range(x.shape[0]):
+            for j in range(1, x.shape[1]):
+                hash = x[i, j]
+                new_x[i, hash % n + 1] += hash
+        return new_x
 
     def standardize(self, x):
         for i in range(1, x.shape[1]):
@@ -86,8 +122,8 @@ class Regression:
 
     def h(self, x_row):
         if self.loss_method == SQUARED:
-            return x_row.dot(self.gradient)
-        return 1.0/(1 + math.exp(-1 * x_row.dot(self.gradient)))
+            return x_row.dot(self.w)
+        return 1.0/(1 + math.exp(-1 * x_row.dot(self.w)))
 
     def loss_in_row(self, x_row, y_val):
         if self.loss_method == SQUARED:
@@ -100,42 +136,38 @@ class Regression:
         ret = 0
         for i in range(x.shape[0]):
             ret += self.loss_in_row(x[i], y[i])
-        l2_elem = self.l2 * sum(self.gradient[j]**2 for j in range(1, len(self.gradient)))
+        l2_elem = self.l2 * sum(self.w[j]**2 for j in range(1, len(self.w)))
         return ret/(2 * x.shape[0]) + l2_elem #TODO before division?
 
     def partial_gradient(self, x, y, point):
         ret = 0.0
         for i in range(x.shape[0]):
             ret += (self.h(x[i]) - y[i]) * x[i, point]
-        l2_elem = self.l2 * 2 * self.gradient[point]
+        l2_elem = self.l2 * 2 * self.w[point]
         return ret/x.shape[0] + l2_elem
 
-    def update_gradient(self, x, y):
-        ret = np.zeros(len(self.gradient))
-        for i in range(len(self.gradient)):
-            ret[i] = self.gradient[i] - \
+    def update_w(self, x, y):
+        ret = np.zeros(len(self.w))
+        for i in range(len(self.w)):
+            ret[i] = self.w[i] - \
                      self.learning_rate * self.partial_gradient(x, y, i)
         return ret
 
     def learn(self):
-        learning_input = self.initial_input
-        learning_output = self.initial_output
-
-        if self.standardization:
-            self.standardize(learning_input)
+        learning_input = self.learning_input
+        learning_output = self.learning_output
 
         for epoch in range(self.n_iter):
 
             train_loss1 = self.loss(learning_input, learning_output)
 
             i_list = list(range(0, len(learning_input), self.batch_size))
-            print i_list
             if self.shuffle:
                 random.shuffle(i_list)
 
             for i in i_list:
                 end = min(i + self.batch_size, len(learning_input))
-                self.gradient = self.update_gradient(learning_input[i:end, :], learning_output[i:end])
+                self.w = self.update_w(learning_input[i:end, :], learning_output[i:end])
                 self.learning_rate *= self.learning_rate_decay
 
             train_loss2 = self.loss(learning_input, learning_output)
@@ -148,20 +180,16 @@ class Regression:
                 print "break after convergence"
                 break
 
-        print learning_input
-        self.predictor = self.gradient
-
     def show_results(self):
         print data
-        print(self.predictor)
         print self.initial_input
         print self.initial_output
-        print self.validation_input
-        print self.validation_output
+        print "learning input"
+        print self.learning_input
         plt.scatter(self.initial_input[:, 1], self.initial_output, color='blue')
         output = np.zeros(len(self.initial_input), dtype=np.float)
         for i in range(len(output)):
-            output[i] = self.h(self.initial_input[i])
+            output[i] = self.h(self.learning_input[i])
         plt.scatter(self.initial_input[:, 1], output, color='red')
         plt.show()
 
